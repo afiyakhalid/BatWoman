310c7e0eed910e5ec3cca4797665787a1a8fb64b
Masood899 <masoodkann899@gmail.com>
2026-07-02 07:18:56 +0530

    added new eception

--- BatWoman-backend/src/main/java/com/BatWoman/BatWoman_backend/exception/GlobalExceptionHandler.java
+++ BatWoman-backend/src/main/java/com/BatWoman/BatWoman_backend/exception/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -92,6 +94,23 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .badRequest()
+                .body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex) {
+
+        ErrorResponse response = new ErrorResponse(
+                OffsetDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                "Request body is missing or malformed.",
+                List.of()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(response);
     }
 
